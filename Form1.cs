3c75733017e9153c6dbc418a9d762c14cfb5876d
EvertonHimself <evertondeoliveira@gmail.com>
2023-05-16 18:16:55 -0300

    Criado os métodos Ler e Escrever.
    Criados os métodos Ler e Escrever para ler e escrever em arquivos de texto.

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace agenda_simples_t5
 {
     public partial class Form1 : Form
     {
+        // Cria um vetor para guardar os contatos.
+        private Contato[] listaDeContatos = new Contato[1];
         public Form1()
         {
             InitializeComponent();
@@ -21,6 +24,46 @@ namespace agenda_simples_t5
         {
             Contato objetoContato = new Contato(txtNome.Text, txtSobrenome.Text, txtTelefone.Text, txtEmail.Text);
             lstContatos.Items.Add(objetoContato.ToString());
+        }
+
+        // Escreve o contato no arquivo de texto.
+        private void Escrever(Contato contato)
+        {
+            StreamWriter escrevedorDeArquivos = new StreamWriter("Contatos.txt");
+            escrevedorDeArquivos.WriteLine(listaDeContatos.Length + 1);
+            escrevedorDeArquivos.WriteLine(contato.PrimeiroNome);
+            escrevedorDeArquivos.WriteLine(contato.Sobrenome);
+            escrevedorDeArquivos.WriteLine(contato.Telefone);
+            escrevedorDeArquivos.WriteLine(contato.Email);
+
+            // Copia os contatos que já estão no arquivo, e re-escreve
+            // eles depois, atualizados.
+            for (int i = 0; i < listaDeContatos.Length; i++)
+            {
+                escrevedorDeArquivos.WriteLine(contato.PrimeiroNome);
+                escrevedorDeArquivos.WriteLine(contato.Sobrenome);
+                escrevedorDeArquivos.WriteLine(contato.Telefone);
+                escrevedorDeArquivos.WriteLine(contato.Email);
+            }
+            escrevedorDeArquivos.Close();
+        }
+
+        // Método para ler os dados do arquivo de texto.
+        private void Ler()
+        {
+            StreamReader leitorDeArquivos = new StreamReader("Contatos.txt");
+            listaDeContatos = new Contato[Convert.ToInt32(leitorDeArquivos.ReadLine())];
+            // Copia os dados do arquivo de texto para o vetor listaDeContatos
+            for (int i = 0; i < listaDeContatos.Length; i++)
+            {
+                listaDeContatos[i] = new Contato();
+                listaDeContatos[i].PrimeiroNome = leitorDeArquivos.ReadLine();
+                listaDeContatos[i].Sobrenome = leitorDeArquivos.ReadLine();
+                listaDeContatos[i].Telefone = leitorDeArquivos.ReadLine();
+                listaDeContatos[i].Email = leitorDeArquivos.ReadLine();
+            }
+
+            leitorDeArquivos.Close();
         }
     }
 }
