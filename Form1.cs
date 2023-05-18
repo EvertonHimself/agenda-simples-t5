ea51ff452f49fc82f8d0bfe8ea229b49de665bfb
EvertonHimself <evertondeoliveira@gmail.com>
2023-05-18 18:10:44 -0300

    Criado o botão ordenar.

--- Form1.cs
+++ Form1.cs
@@ -24,8 +24,13 @@ namespace agenda_simples_t5
         private void btnAddContato_Click(object sender, EventArgs e)
         {
             // Cria um objeto objetoContato,com os dados do formulário.
-            Contato objetoContato = new Contato(txtNome.Text, txtSobrenome.Text, txtTelefone.Text, txtEmail.Text);
-            lstContatos.Items.Add(objetoContato);
+            Contato objetoContato = new Contato(txtNome.Text, 
+                txtSobrenome.Text, txtTelefone.Text, txtEmail.Text);
+            Escrever(objetoContato);
+            Ordenar();
+            Ler();
+            AtualizarLista();
+            LimparFormulario();
         }
 
         // Escreve o contato no arquivo de texto.
@@ -42,10 +47,10 @@ namespace agenda_simples_t5
             // eles depois, atualizados.
             for (int i = 0; i < listaDeContatos.Length; i++)
             {
-                escrevedorDeArquivos.WriteLine(contato.PrimeiroNome);
-                escrevedorDeArquivos.WriteLine(contato.Sobrenome);
-                escrevedorDeArquivos.WriteLine(contato.Telefone);
-                escrevedorDeArquivos.WriteLine(contato.Email);
+                escrevedorDeArquivos.WriteLine(listaDeContatos[i].PrimeiroNome);
+                escrevedorDeArquivos.WriteLine(listaDeContatos[i].Sobrenome);
+                escrevedorDeArquivos.WriteLine(listaDeContatos[i].Telefone);
+                escrevedorDeArquivos.WriteLine(listaDeContatos[i].Email);
             }
             escrevedorDeArquivos.Close();
         }
@@ -66,6 +71,56 @@ namespace agenda_simples_t5
             }
 
             leitorDeArquivos.Close();
+        }
+
+        private void AtualizarLista()
+        {
+            // Apaga os dados da lista que está sendo exibida.
+            lstContatos.Items.Clear();
+            for (int i = 0; i < listaDeContatos.Length; i++)
+            {
+                lstContatos.Items.Add(listaDeContatos[i].ToString());
+            }
+        }
+
+        private void LimparFormulario()
+        {
+            txtNome.Text = string.Empty;
+            txtSobrenome.Text = string.Empty;
+            txtTelefone.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ler();
+            AtualizarLista();
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            Ordenar();
+            AtualizarLista();
+        }
+        private void Ordenar()
+        {
+            Contato temporario;
+            bool trocar;
+            do
+            {
+                trocar = false;
+                for (int i = 0; i < listaDeContatos.Length - 1; i++)
+                {
+                    if (listaDeContatos[i].PrimeiroNome.
+                        CompareTo(listaDeContatos[i + 1].PrimeiroNome) > 0)
+                    {
+                        temporario = listaDeContatos[i];
+                        listaDeContatos[i] = listaDeContatos[i + 1];
+                        listaDeContatos[i + 1] = temporario;
+                        trocar = true;
+                    }
+                }
+            } while (trocar == true);
         }
     }
 }
